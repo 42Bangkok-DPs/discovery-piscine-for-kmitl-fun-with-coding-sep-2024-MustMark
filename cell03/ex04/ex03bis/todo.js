d2a0673c44814e22e7813aef97545aae78b0c674
Peerawat Ingkhasantatikul <136987643+MustMark@users.noreply.github.com>
2024-09-12 21:31:42 +0700

    upload ex04

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    const $ft_list = $("#ft_list");
+    const $NewButton = $("#NewButton");
+
+    LoadTodo();
+
+    $NewButton.on("click", function() {
+        const todoText = prompt("Enter a new TO-DO:");
+        if (todoText) {
+            AddTodo(todoText);
+        }
+    });
+
+    function AddTodo(text) {
+        const $todoDiv = $("<div></div>").text(text).addClass("todo-item");
+        $todoDiv.on("click", function() {
+            if (confirm("Are you sure to remove this TO-DO?")) {
+                $todoDiv.remove();
+                SaveTodo();
+            }
+        });
+        $ft_list.prepend($todoDiv);
+        SaveTodo();
+    }
+
+    function SaveTodo() {
+        const todo = $ft_list.children().map(function() {
+            return $(this).text();
+        }).get();
+        document.cookie = `todo=${JSON.stringify(todo)}; path=/;`;
+    }
+
+    function LoadTodo() {
+        const cookies = document.cookie.split("; ").find(row => row.startsWith("todo="));
+        if (cookies) {
+            const todo = JSON.parse(cookies.split("=")[1]);
+            $.each(todo.reverse(), function(index, value) {
+                AddTodo(value);
+            });
+        }
+    }
+});
